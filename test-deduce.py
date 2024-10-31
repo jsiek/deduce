39a27b5b4c0b9a8d7f4d8465989710365b6c782a
Shulin Gonsalves <shugons@iu.edu>
2024-10-31 10:31:40 -0400

    Testing script

---
+++ test-deduce.py
@@ -0,0 +1,95 @@
+import os
+import sys
+
+parsers = ['--recursive-descent', '--lalr']
+
+lib_dir = './lib'
+pass_dir = './test/should-pass'
+error_dir = './test/should-error'
+
+def test_deduce(deduce_call, path, expected_return = 0, extra_arguments=""):
+    deduce_call += ' ' + path
+    for parser in parsers:
+        call = deduce_call + ' ' + parser + ' ' + extra_arguments
+        print('Testing:', call)
+        return_code = os.system(call) // 256 # Why does it multiply the return code by 256???
+        if return_code != expected_return:
+            print(call, 'expected return code', expected_return, 'but got', return_code)
+            exit(1)
+
+def generate_deduce_errors(deduce_call, path):
+    # We don't pass in the --error flag so we can generate error messages
+    # However, that means we can't levarage deduces already existed directory stuff
+    # So we manually do it here
+
+    if os.path.isfile(path):
+        test_deduce(deduce_call, path, 1, '> ' + path + '.err')
+    elif os.path.isdir(path):
+        if path[-1] != '/' or path[-1] != '\\': # Windows moment
+            path += '/'
+        for file in os.listdir(path):
+            if os.path.isfile(path + file):
+                if file[-3:] == '.pf':
+                    generate_deduce_errors(deduce_call, path + file)
+            elif os.path.isdir(path + file):
+                # TODO: recursive directories
+                pass
+    else:
+        print(path, 'was not found!')
+        exit(1)
+
+def test_deduce_errors(deduce_call, path):
+    if os.path.isfile(path):
+        if not os.path.isfile(path + '.err'):
+            print("Couldn't find an expected error for", path)
+            print("Did you mean to generate it? If so, use generate_deduce_errors")
+            exit(1)
+
+        temp_file  = './actual_error.tmp' # THIS NEEDS TO BE HARDCODED AND CONSTANT
+        test_deduce(deduce_call, path + ' > ' + temp_file, 1)
+        ret_code = os.system('diff ' + path + '.err ' + temp_file)
+        os.remove(temp_file) # PLEASE MAKE SURE THIS IS HARDCODED AND CONSTANT!!!!!!
+
+        if ret_code != 0:
+            print("The error message for", path, "has changed!!!!")
+            exit(1)
+    else:
+        if path[-1] != '/' or path[-1] != '\\': # Windows moment
+            path += '/'
+        for file in os.listdir(path):
+            if os.path.isfile(path + file):
+                if file[-3:] == '.pf':
+                    test_deduce_errors(deduce_call, path + file)
+            elif os.path.isdir(path + file):
+                # TODO: recursive directories?
+                pass
+
+if __name__ == "__main__":
+    # Check command line arguments
+    extra_arguments = []
+    generate_errors = False
+
+    already_processed_next = False
+    for i in range(1, len(sys.argv)):
+        if already_processed_next:
+            already_processed_next = False
+            continue
+    
+        argument = sys.argv[i]
+        if argument == '--regenerate-errors':
+            generate_errors = True
+
+        extra_arguments.append(argument)
+    
+    python_path = os.popen("command -v python3").read()[0: -1] # strip the newline character with the splicing
+    deduce_call = python_path + " ./deduce.py " + " --dir " + lib_dir + " ".join(extra_arguments)
+
+    # Yes there is a chance to optimize this by doing testing the lib directory one time, then adding it to uniquifed modules
+    # HOWEVER, I want to make sure the testing suite runs deduce as it normally would
+    # So we don't "fix" something in the suite then have it run into errors in actual usage and be like aww shucks
+    if generate_errors:
+        generate_deduce_errors(deduce_call, error_dir)
+
+    test_deduce(deduce_call, lib_dir)
+    test_deduce(deduce_call, pass_dir)
+    test_deduce_errors(deduce_call, error_dir)
