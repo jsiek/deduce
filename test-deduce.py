6c46b49313a9f23a1d6eb4f2e260f305b05566e1
Jeremy G. Siek <jeremy.siek@gmail.com>
2024-11-06 21:13:39 -0500

    don't check error messages from LALR parser

--- test-deduce.py
+++ test-deduce.py
@@ -7,7 +7,7 @@ lib_dir = './lib'
 pass_dir = './test/should-pass'
 error_dir = './test/should-error'
 
-def test_deduce(deduce_call, path, expected_return = 0, extra_arguments=""):
+def test_deduce(parsers, deduce_call, path, expected_return = 0, extra_arguments=""):
     deduce_call += ' ' + path
     for parser in parsers:
         call = deduce_call + ' ' + parser + ' ' + extra_arguments
@@ -23,8 +23,7 @@ def generate_deduce_errors(deduce_call, path):
     # So we manually do it here
 
     if os.path.isfile(path):
-        # TODO: Make sure both paresers produce the same error
-        test_deduce(deduce_call, path, 1, '> ' + path + '.err') # this will test both parsers, which should both error
+        test_deduce(['--recursive-descent'], deduce_call, path, 1, '> ' + path + '.err')
     elif os.path.isdir(path):
         if path[-1] != '/' or path[-1] != '\\': # Windows moment
             path += '/'
@@ -46,13 +45,14 @@ def test_deduce_errors(deduce_call, path):
             print("Did you mean to generate it? If so, use generate_deduce_errors")
             exit(1)
 
-        temp_file  = './actual_error.tmp' # THIS NEEDS TO BE HARDCODED AND CONSTANT
-        test_deduce(deduce_call, path + ' > ' + temp_file, 1)
+        temp_file  = './actual_error.tmp'
+        test_deduce(['--recursive-descent'], deduce_call, path + ' > ' + temp_file, 1)
         ret_code = os.system('diff ' + path + '.err ' + temp_file)
-        os.remove(temp_file) # PLEASE MAKE SURE THIS IS HARDCODED AND CONSTANT!!!!!!
 
-        if ret_code != 0:
-            print("The error message for", path, "has changed!!!!")
+        if ret_code == 0:
+            os.remove(temp_file)
+        else:
+            print("The error message for", path, "has changed! See actual_error.tmp")
             exit(1)
     else:
         if path[-1] != '/' or path[-1] != '\\': # Windows moment
@@ -111,10 +111,6 @@ if __name__ == "__main__":
     
     deduce_call = python_path + " ./deduce.py " + " --dir " + lib_dir + " ".join(extra_arguments)
 
-    # Yes there is a chance to optimize this by doing testing the lib directory one time, then adding it to uniquifed modules
-    # HOWEVER, I want to make sure the testing suite runs deduce as it normally would
-    # So we don't "fix" something in the suite then have it run into errors in actual usage and be like aww shucks
-
     if generate_errors:
         print('Regenerating ALL errors')
         generate_deduce_errors(deduce_call, error_dir)
@@ -125,13 +121,13 @@ if __name__ == "__main__":
             generate_errors = True # So we don't run ALL tests
 
     if test_lib:
-        test_deduce(deduce_call, lib_dir)
+        test_deduce(parsers, deduce_call, lib_dir)
     if test_passable:
-        test_deduce(deduce_call, pass_dir)
+        test_deduce(parsers, deduce_call, pass_dir)
     if test_errors:
         test_deduce_errors(deduce_call, error_dir)
 
     if not (test_lib or test_passable or test_errors or generate_errors):
-        test_deduce(deduce_call, lib_dir)
-        test_deduce(deduce_call, pass_dir)
+        test_deduce(parsers, deduce_call, lib_dir)
+        test_deduce(parsers, deduce_call, pass_dir)
         test_deduce_errors(deduce_call, error_dir)
