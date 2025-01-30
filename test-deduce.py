cef18e1f9a739ef4dcf0924c78d07254948732e0
Shulin Gonsalves <shugons@iu.edu>
2025-01-30 16:00:08 -0500

    remove redundant == 1

--- test-deduce.py
+++ test-deduce.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     if test_errors:
         test_deduce_errors(deduce_call, error_dir)
 
-    if not (test_lib or test_passable or test_errors or test_site) == 1: # run everything
+    if not (test_lib or test_passable or test_errors or test_site): # run everything
         # test
         test_deduce(parsers, deduce_call, [lib_dir, pass_dir, site_dir + '/home_example1.pf',
                                                               site_dir + '/home_example2.pf',
