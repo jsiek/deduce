1e07268ee637ce2f1188d6387e9647e39016590d
Jeremy Siek <jeremy.siek@gmail.com>
2025-07-29 11:45:24 -0400

    another anon

--- gh_pages/doc/lib_generate.py
+++ gh_pages/doc/lib_generate.py
@@ -281,7 +281,8 @@ known_tokens = {
     '__ANON_24': 'operator', # \\->
     '__ANON_25': 'prim', # 0
     '__ANON_26': 'prim',    # \.-
-    '__ANON_28': 'operator', # ≲ or ≈ ?
+    '__ANON_27': 'operator', # ≲
+    '__ANON_28': 'operator', # ≈
     '__ANON_3': 'operator', # /=
     '__ANON_4': 'operator', # ≤
     '__ANON_5': 'operator', # <=
